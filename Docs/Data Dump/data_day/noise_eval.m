47eb202a06adad3bad640d68cb3593edcb1e2f1d
Kartik Gohil <kartik_gohil@hotmail.com>
2015-06-15 23:29:53 +0100

    rail noise graphs done

--- Docs/Data Dump/data_day/noise_eval.m
+++ Docs/Data Dump/data_day/noise_eval.m
@@ -60,6 +60,37 @@ grid minor;
 xlabel('Time (seconds)');
 ylabel('Amplitude (Volts)');
 
+%% RAIL NOISE 3
+%COLUMN 1: SECONDS
+%COLUMN 2: VOLTS
+B = csvread('scope_14.csv');
+start = min(find(B(:,1)>-4e-3));
+last = max(find(B(:,1)<4e-3));
+A = B; %everything
+%A = B(100:900,:); %everything
+numsamp = size(A,1);
+T = (A(end,1)-A(1,1))/numsamp;
+fs = 1/T;
+
+%subplot(2,1,1)
+plot(A(:,1),A(:,2))
+xlim([A(1,1),A(end,1)]);
+ylim([4.75 4.79]);
+grid minor;
+xlabel('Time (seconds)');
+ylabel('Amplitude (Volts)');
+%subplot(2,1,2)
+diff = 100;
+for i=diff+1:numsamp
+   sig(i) = mean(A(i-diff:i,2)); 
+end
+sig(sig<4.772)=0;
+sig(sig~=0)=1;
+hold on
+plot(A(:,1),(sig/50)+4.76, 'Color', 'r');
+%ylim([4.768 4.776]);
+hold off
+
 
 %% SIGNAL NOISE 1
 %COLUMN 1: SECONDS
@@ -135,3 +166,44 @@ ylim([1e-2 1e2]);
 grid minor;
 xlabel('Frequency (Hz)');
 ylabel('Amplitude (dB)');
+
+%% SIGNAL NOISE 3
+%COLUMN 1: SECONDS
+%COLUMN 2: VOLTS
+B = csvread('scope_14.csv');
+%A = B(800:900,:);%idle
+A = B(82240:82500);%ringtap
+numsamp = size(A,1);
+fs = 44100/16;
+T = 1/fs;
+
+subplot(2,1,1)
+time = [0:T:numsamp/fs];
+plot(time,A)
+%xlim([A(1,1),A(end,1)]);
+%ylim([1 4]);
+grid minor;
+xlabel('Time (seconds)');
+ylabel('Amplitude (Volts)');
+
+subplot(2,1,2)
+freqlim = [50:fs/numsamp:fs/2];
+freqax = round(freqlim*numsamp/fs);
+I = find(freqax==0);
+freqax(I) = 1;
+A_freq = abs(fft(A(:,2)));
+loglog(freqlim,A_freq(freqax));
+%hold on;
+%temp = zeros(1,length(freqlim));
+temp(max(find(freqlim<500))) = 100;
+%stem(freqlim,temp,'LineStyle','--','Marker', 'none');
+%str1 = '490Hz';
+%text(350,0.1,str1,'Color','r')
+%hold off;
+%ylim([0,5]);
+xlim([freqlim(1), freqlim(end)]);
+ylim([1e-2 1e2]);
+grid minor;
+xlabel('Frequency (Hz)');
+ylabel('Amplitude (dB)');
+5026.63115845539
